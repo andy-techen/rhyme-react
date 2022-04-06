d08d8d2d94086e179b42547a9997c692e0049478
Andy Chen <andy27479@gmail.com>
2022-04-06 17:14:33 -0400

    added github link and loading state

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ function App() {
   const [savedWords, setSavedWords] = useState([]);
   const [term, setTerm] = useState('');
   const [type, setType] = useState('');
-  const [output, setOutput] = useState('...loading');
+  const [output, setOutput] = useState('none');
+  const [loading, setLoading] = useState(false);
 
   return (
     <Container>
       <h1>Rhyme Finder (579 Problem Set 6)</h1>
+      <a href="https://github.com/andy-techen/rhyme-react" target="_blank">Github Repo</a>
       <SavedWords words={savedWords} />
-      <SearchBar setOutput={setOutput} setType={setType} setTerm={setTerm} />
-      <WordOutput output={output} type={type} term={term} onSave={(word) => setSavedWords((savedWords) => [...savedWords, word])} />
+      <SearchBar setOutput={setOutput} setType={setType} setTerm={setTerm} setLoading={setLoading} />
+      <WordOutput loading={loading} output={output} type={type} term={term} onSave={(word) => setSavedWords((savedWords) => [...savedWords, word])} />
     </Container>
   );
 }
